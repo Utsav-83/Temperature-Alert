fcfc3db64405730d8995e9f294d442eef2a34874
Utsav-83 <utsav8391@gmail.com>
2025-10-04 09:51:19 +0530

    Saving Temperature program

---
+++ temp.py
@@ -0,0 +1,12 @@
+# Temperature Check Program
+
+# Input temperature in Celsius
+temp = float(input("Enter temperature in Celsius: "))
+
+# Conditions
+if temp < 20:
+    print("It's Cold ❄")
+elif 20 <= temp <= 30:
+    print("It's Normal 🙂")
+else:
+    print("It's Hot 🔥")
